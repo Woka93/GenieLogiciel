5e4e93896e1b33505ce8cc38acf173184bfcfe86
AkagiCritMagnet <vivaldi_du_14@hotmail.com>
2015-11-23 11:13:01 +0100

    Correction de l'inversion des X et Y

--- ProjetRobot/src/ia/Calcul.java
+++ ProjetRobot/src/ia/Calcul.java
@@ -12,7 +12,7 @@ public class Calcul {
 	
 	public boolean Aligner(Robot player1, Robot player2) {
 		
-		if (player1.getPositionX() == player2.getPositionX() || player1.getPositionY() == player2.getPositionY()) {
+		if (player1.getPositionY() == player2.getPositionY() || player1.getPositionX() == player2.getPositionX()) {
 			return true;
 		}
 		return false;
@@ -22,16 +22,16 @@ public class Calcul {
 		
 		boolean oriente = false;
 		
-		if ((pion1.getOriente() == 'h') && (pion1.getPositionY() > pion2.getPositionY())) {
+		if ((pion1.getOriente() == 'h') && (pion1.getPositionX() > pion2.getPositionX())) {
 			oriente = true;
 		}
-		if ((pion1.getOriente() == 'b') && (pion1.getPositionY() < pion2.getPositionY())) {
+		if ((pion1.getOriente() == 'b') && (pion1.getPositionX() < pion2.getPositionX())) {
 			oriente = true;
 		}
-		if ((pion1.getOriente() == 'g') && (pion1.getPositionX() > pion2.getPositionX())) {
+		if ((pion1.getOriente() == 'g') && (pion1.getPositionY() > pion2.getPositionY())) {
 			oriente = true;
 		}
-		if ((pion1.getOriente() == 'd') && (pion1.getPositionX() < pion2.getPositionX())) {
+		if ((pion1.getOriente() == 'd') && (pion1.getPositionY() < pion2.getPositionY())) {
 			oriente = true;
 		}
 		return oriente;
@@ -44,16 +44,16 @@ public class Calcul {
 		//dist = Math.abs(player1.getPositionX()-player2.getPositionX()) + Math.abs(player1.getPositionY()-player2.getPositionY());
 		
 		for (int i = 0; i < ListeMissile.size(); i++) {
-			if (player1.getPositionY() == ListeMissile.get(i).getPositionY()) {
-				if (Math.abs(player1.getPositionX() - ListeMissile.get(i).getPositionX()) <= 2) {
+			if (player1.getPositionX() == ListeMissile.get(i).getPositionX()) {
+				if (Math.abs(player1.getPositionY() - ListeMissile.get(i).getPositionY()) <= 2) {
 					if (player != ListeMissile.get(i).getPlayerRobot()) {
 						return i;
 					}
 				}
 			}
 
-			if (player1.getPositionX() == ListeMissile.get(i).getPositionX()) {
-				if (Math.abs(player1.getPositionY() - ListeMissile.get(i).getPositionY()) <= 2) {
+			if (player1.getPositionY() == ListeMissile.get(i).getPositionY()) {
+				if (Math.abs(player1.getPositionX() - ListeMissile.get(i).getPositionX()) <= 2) {
 					if (player != ListeMissile.get(i).getPlayerRobot()) {
 						return i;
 					}
@@ -77,9 +77,9 @@ public class Calcul {
 	
 	public boolean ProcheLignesouColonnes (Robot player1, Robot player2) {
 		
-		if (Math.abs(player1.getPositionX()-player2.getPositionX()) > Math.abs(player1.getPositionY()-player2.getPositionY())) {
+		if (Math.abs(player1.getPositionY()-player2.getPositionY()) > Math.abs(player1.getPositionY()-player2.getPositionY())) {
 			return true; 		// proche des lignes
-		} else if (Math.abs(player1.getPositionX()-player2.getPositionX()) == Math.abs(player1.getPositionY()-player2.getPositionY())) {
+		} else if (Math.abs(player1.getPositionY()-player2.getPositionY()) == Math.abs(player1.getPositionY()-player2.getPositionY())) {
 			if (player1.getOriente() == 'h' || player1.getOriente() == 'b') {
 				return true;	// on garde la direction actuelle
 			}
@@ -93,13 +93,13 @@ public class Calcul {
 		case 'g' :
 		case 'd' : 	player1.ChangerOrientationRobot(player1, 1);
 					break;
-		case 'h' :	if (player1.getPositionY() > player2.getPositionY()) {
+		case 'h' :	if (player1.getPositionX() > player2.getPositionX()) {
 						player1.DeplacementAV();
 					} else {
 						player1.DeplacementAR();
 					}
 					break;
-		case 'b' : 	if (player1.getPositionY() > player2.getPositionY()) {
+		case 'b' : 	if (player1.getPositionX() > player2.getPositionX()) {
 						player1.DeplacementAR();
 					} else {
 						player1.DeplacementAV();
@@ -114,13 +114,13 @@ public class Calcul {
 		case 'h' :
 		case 'b' : 	player1.ChangerOrientationRobot(player1, 1);
 					break;
-		case 'g' :	if (player1.getPositionX() > player2.getPositionX()) {
+		case 'g' :	if (player1.getPositionY() > player2.getPositionY()) {
 						player1.DeplacementAV();
 					} else {
 						player1.DeplacementAR();
 					}
 					break;
-		case 'd' : 	if (player1.getPositionX() > player2.getPositionX()) {
+		case 'd' : 	if (player1.getPositionY() > player2.getPositionY()) {
 						player1.DeplacementAR();
 					} else {
 						player1.DeplacementAV();
@@ -131,16 +131,16 @@ public class Calcul {
 	
 	public int DirectiondeEnnemi (Robot player1, Robot player2) {
 		
-		if (player1.getPositionY() > player2.getPositionY()) {
+		if (player1.getPositionX() > player2.getPositionX()) {
 			return 0; //haut
 		}
-		if (player1.getPositionY() < player2.getPositionY()) {
+		if (player1.getPositionX() < player2.getPositionX()) {
 			return 1; //bas
 		}
-		if (player1.getPositionX() > player2.getPositionX()) {
+		if (player1.getPositionY() > player2.getPositionY()) {
 			return 2; //gauche
 		}
-		if (player1.getPositionX() < player2.getPositionX()) {
+		if (player1.getPositionY() < player2.getPositionY()) {
 			return 3; //droite
 		}
 		return -1;
